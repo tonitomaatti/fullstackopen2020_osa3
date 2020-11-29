0378283cc887c3331d9c31a084015f149fcf9fce
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-11-29 02:48:13 +0200

    3.4 tiedon poisto

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
   res.send(
     `<p>Phonebook has info for ${persons.length} people</p>
