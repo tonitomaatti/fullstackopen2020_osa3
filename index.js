575fc4316983e0658a2ace6b5f0962178232c007
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-11-29 02:40:30 +0200

    3.3 yksittäinen tieto

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -30,6 +31,17 @@ app.get('/', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    res.json(person)  
+  } else {
+    res.status(404).end()
+  }
 })
 
 app.get('/info', (req, res) => {
