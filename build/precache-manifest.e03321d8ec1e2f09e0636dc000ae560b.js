880c83bffcce49da0221c18d5ed56af401d3e284
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-08 20:48:43 +0200

    3.11: front page mukana

---
+++ build/precache-manifest.e03321d8ec1e2f09e0636dc000ae560b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea38ca9bff53647f89fc359d9b76dc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81f6fb08f4e80f5d9136",
+    "url": "/static/css/main.b0a7673b.chunk.css"
+  },
+  {
+    "revision": "c574ff2531d61c703e37",
+    "url": "/static/js/2.16e154f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16e154f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f6fb08f4e80f5d9136",
+    "url": "/static/js/main.536fe316.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body' , (req, res) => {
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
