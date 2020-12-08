efbc0e3ac04dc0ba5eab257b70d5db187627db57
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-08 18:41:53 +0200

    3.8: body morganin logissa

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+
+morgan.token('body' , (req, res) => {
+  return JSON.stringify(req.body)
+})
+
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
